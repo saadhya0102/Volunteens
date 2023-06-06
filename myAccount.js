248b4e837712ef5e2fe4243ecd67c5ff6419dedd
laylaliuuu <24lliu@ndahingham.com>
2023-06-06 00:05:27 -0400

    My account
    trying

---
+++ myAccount.js
@@ -0,0 +1,6 @@
+const regbtn = document.getElementById('reg-btn');
+
+regbtn.addEventListener('click',function(){
+    document.getElementById('register-div').style.display="inline";
+    document.getElementById('login-div').style.display="none";
+});
